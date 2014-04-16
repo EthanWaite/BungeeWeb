dfc092a0edf043e51931f0f627053ac3ddd998b2
Dead-i <ethan.waite@btinternet.com>
2014-04-16 21:48:28 +0100

    Add "getplayer" API function and include GSON
    Currently does not require authentication!

--- src/io/github/dead_i/bungeeweb/WebHandler.java
+++ src/io/github/dead_i/bungeeweb/WebHandler.java
@@ -1,6 +1,7 @@
 package io.github.dead_i.bungeeweb;
 
 import com.google.common.io.ByteStreams;
+import com.google.gson.Gson;
 import net.md_5.bungee.api.plugin.Plugin;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
@@ -10,6 +11,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class WebHandler extends AbstractHandler {
     private Plugin plugin;
@@ -27,8 +31,38 @@ public class WebHandler extends AbstractHandler {
         if (target.equals("/")) target = "/index.html";
         String[] path = target.split("/");
 
-        if (target.startsWith("/api/")) {
-            //TODO api
+        if (path.length > 2 && path[1].equalsIgnoreCase("api")) {
+            baseReq.setHandled(true);
+            if (path[2].equalsIgnoreCase("getplayer")) {
+                ArrayList<String> conditions = new ArrayList<String>();
+
+                String player = req.getParameter("uuid");
+                if (player == null) conditions.add("`uuid`=?");
+
+                String from = req.getParameter("time");
+                if (from != null) {
+                    int time = 0;
+                    try {
+                        time = Integer.parseInt(from);
+                    }catch (NumberFormatException ignored) {}
+                    if (time != 0) conditions.add("`time`=?");
+                }
+
+                String cond = "";
+                if (conditions.size() > 0) {
+                    cond = "WHERE ";
+                    for (String s : conditions) {
+                        cond += s + " AND ";
+                    }
+                    cond = cond.substring(0, cond.length() - 4);
+                }
+
+                try {
+                    BungeeWeb.getDatabase().prepareStatement("SELECT * FROM `" + BungeeWeb.getConfig().getString("database.prefix") + "logs` " + cond + "LIMIT 50");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }else{
             InputStream stream = plugin.getResourceAsStream("web/" + path[1]);
             if (stream != null) {
