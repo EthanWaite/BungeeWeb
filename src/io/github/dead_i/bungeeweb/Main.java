ea78e83f6aafdd3e04f2b654cec647cd682c139e
Dead-i <ethan.waite@btinternet.com>
2014-04-16 11:32:25 +0100

    Add configuration handler

--- src/io/github/dead_i/bungeeweb/Main.java
+++ src/io/github/dead_i/bungeeweb/Main.java
@@ -1,13 +1,36 @@
 package io.github.dead_i.bungeeweb;
 
+import com.google.common.io.ByteStreams;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.util.log.Log;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Main extends Plugin {
     public static Server server;
+    public static Configuration config;
 
     public void onEnable() {
+        // Get configuration
+        if (!getDataFolder().exists()) getDataFolder().mkdir();
+        File configFile = new File(getDataFolder(), "config.yml");
+        try {
+            if (!configFile.exists()) {
+                configFile.createNewFile();
+                ByteStreams.copy(getResourceAsStream("config.yml"), new FileOutputStream(configFile));
+            }
+            config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         // Setup the server
         server = new Server(8080);
         server.setHandler(new WebHandler(this));
