49d8b00d81c535514077fc8ea904d3d416c99160
Dead-i <ethan.waite@btinternet.com>
2014-04-16 17:36:42 +0100

    Add login listener

---
+++ src/io/github/dead_i/bungeeweb/listeners/PostLoginListener.java
@@ -0,0 +1,15 @@
+package io.github.dead_i.bungeeweb.listeners;
+
+import io.github.dead_i.bungeeweb.BungeeWeb;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+
+public class PostLoginListener implements Listener {
+    @EventHandler
+    public void onPostLogin(PostLoginEvent event) {
+        ProxiedPlayer p = event.getPlayer();
+        BungeeWeb.log(p, 3, p.getAddress().getHostString());
+    }
+}
