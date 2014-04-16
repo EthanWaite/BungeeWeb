a2db8a5fe0d9f02e423f1b65128ddf9eee6b549a
Dead-i <ethan.waite@btinternet.com>
2014-04-16 17:53:43 +0100

    Add kick listener

---
+++ src/io/github/dead_i/bungeeweb/listeners/ServerKickListener.java
@@ -0,0 +1,14 @@
+package io.github.dead_i.bungeeweb.listeners;
+
+import io.github.dead_i.bungeeweb.BungeeWeb;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.event.ServerKickEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+
+public class ServerKickListener implements Listener {
+    @EventHandler
+    public void onServerKick(ServerKickEvent event) {
+        BungeeWeb.log(event.getPlayer(), 5, event.getPlayer().getServer().getInfo().getName() + ": " + BaseComponent.toPlainText(event.getKickReasonComponent()))
+    }
+}
