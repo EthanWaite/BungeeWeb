44493bf7efa0017c67f0b879fcd3bf14514ab9b0
Dead-i <ethan.waite@btinternet.com>
2014-04-16 17:29:12 +0100

    Add chat listener

---
+++ src/io/github/dead_i/bungeeweb/listeners/ChatListener.java
@@ -0,0 +1,18 @@
+package io.github.dead_i.bungeeweb.listeners;
+
+import io.github.dead_i.bungeeweb.Main;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.ChatEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+
+public class ChatListener implements Listener {
+    @EventHandler
+    public void onChat(ChatEvent event) {
+        String msg = event.getMessage();
+        ProxiedPlayer p = (ProxiedPlayer) event.getSender();
+        int type = 1;
+        if (msg.startsWith("/")) type = 2;
+        Main.log((ProxiedPlayer) event.getSender(), type, msg);
+    }
+}
