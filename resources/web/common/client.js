e15732f4bbf0bb4fc73d3e7a13cd9a8118fa46e7
Dead-i <ethan.waite@btinternet.com>
2014-04-22 22:10:41 +0100

    Add frontend dashboard server list
    Includes several frontend stying changes and a few changes in the way
    the js works.

--- resources/web/common/client.js
+++ resources/web/common/client.js
@@ -5,26 +5,46 @@
 
 // Login handler
 $('.login form').submit(function(e) {
-	e.preventDefault();
+	e.preventDefault();	
 	$('.login .error').fadeOut(200);
 	$.post('/login/', $(this).serialize()).done(function(data) {
-		try {
-			var json = JSON.parse(data);
-		} catch(e) {
-			error();
-			return;
-		}
-		
-		if (json.status == 1) {
-			$('.login').fadeOut(1000, function() {
-				$('.navbar').slideDown(800);
-				$('div[data-page="home"]').fadeIn(1000);
-			});
-		}else{
-			$('.login .error').slideDown(500);
-		}
+		parse(data, function(json) {
+			if (json.status == 1) {
+				$('.login').fadeOut(1000, function() {
+					$('.navbar').slideDown(800);
+					$('#dashboard').fadeIn(1000);
+					loadDashboard();
+				});
+			}else{
+				$('.login .error').slideDown(500);
+			}
+		});
 	});
 });
+
+// Dashboard loader
+function loadDashboard() {
+	$.get('/api/listservers', function(data) {
+		parse(data, function(json) {
+			var i = 0;
+			for (server in json) {
+				$('#dashboard .servers ul').append('<li>' + server + '<span class="badge">' + json[server] + '</span></li>');
+				i++;
+			}
+			$('#dashboard .servers h1 span').text(i + ' servers');
+		});
+	});
+}
+
+// JSON handler
+function parse(data, cb) {
+	try {
+		var json = JSON.parse(data);
+		cb(json);
+	} catch(err) {
+		error();
+	}
+}
 
 // Error handler
 function error() {
