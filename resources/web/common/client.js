d4483c1bdc73e0f6b580ab5911895098a4e28c6c
Dead-i <ethan.waite@btinternet.com>
2014-04-22 18:44:38 +0100

    Frontend login system and styling changes

---
+++ resources/web/common/client.js
@@ -0,0 +1,32 @@
+/**
+ * BungeeWeb
+ * https://github.com/Dead-i/BungeeWeb
+ */
+
+// Login handler
+$('.login form').submit(function(e) {
+	e.preventDefault();
+	$('.login .error').fadeOut(200);
+	$.post("index.html", $(this).serialize()).done(function(data) {
+		try {
+			var json = JSON.parse(data);
+		} catch(e) {
+			error();
+			return;
+		}
+		
+		if (json.status == 1) {
+			$('.login').fadeOut(1000, function() {
+				$('.navbar').slideDown(800);
+				$('div[data-page="home"]').fadeIn(1000);
+			});
+		}else{
+			$('.login .error').slideDown(500);
+		]
+	});
+});
+
+// Error handler
+function error() {
+	$('.errorbar').slideDown(800).delay(4000).slideUp(800);
+}
