3e2ed6d7256076ad023a9b1c2e64439e4e5a7c26
Dead-i <ethan.waite@btinternet.com>
2014-04-23 22:30:25 +0100

    Dashboard logs frontend

--- resources/web/common/client.js
+++ resources/web/common/client.js
@@ -24,14 +24,25 @@ $('.login form').submit(function(e) {
 
 // Dashboard loader
 function loadDashboard() {
+	var players = 0;
 	$.get('/api/listservers', function(data) {
 		parse(data, function(json) {
 			var i = 0;
 			for (server in json) {
 				$('#dashboard .servers ul').append('<li>' + server + '<span class="badge">' + json[server] + '</span></li>');
+				players = players + json[server];
 				i++;
 			}
 			$('#dashboard .servers h1 span').text(i + ' servers');
+			
+			$.get('/api/getlogs?limit=5', function(data) {
+				parse(data, function(json) {
+					for (item in json) {
+						$('#dashboard .servers ul').append('<li>' + formatLog(json[item]) + '</li>');
+					}
+					$('#dashboard .logs h1 span').text(players + ' players');
+				});
+			});
 		});
 	});
 }
@@ -44,6 +55,41 @@ function parse(data, cb) {
 	} catch(err) {
 		error();
 	}
+}
+
+// Log handler
+function formatLog(log) {
+	switch(log.type) {
+		case 1:
+			var msg = '{PLAYER}: {CONTENT}';
+			break;
+		
+		case 2:
+			var msg = '{PLAYER} ran the command {CONTENT}';
+			break;
+		
+		case 3:
+			var msg = '{PLAYER} joined the proxy';
+			break;
+		
+		case 4:
+			var msg = '{PLAYER} disconnected from the proxy';
+			break;
+		
+		case 5:
+			var msg = '{PLAYER} was kicked from {CONTENT}';
+			break;
+		
+		case 6:
+			var msg = '{PLAYER} switched to {CONTENT}';
+			break;
+		
+		default:
+			var msg = '{CONTENT}';
+	}
+	
+	return msg.replace('{PLAYER}', log.username)
+		.replace('{CONTENT}', log.content);
 }
 
 // Error handler
