585c528ced8846dedc4e31fee4e64d6d09f9cf92
Dead-i <ethan.waite@btinternet.com>
2014-09-07 11:04:36 +0100

    Add Players auto-refresh & fix minor search bug

--- resources/web/js/client.players.js
+++ resources/web/js/client.players.js
@@ -7,25 +7,34 @@ pages.players = (function() {
 			var i = 0;
 			for (server in data) {
 				if (i % 3 == 0) $('#players').append('<div class="row"></div>');
-				$('#players .row').last().append('<div class="server"><h4>' + server + '</h4></div>');
-				for (uuid in data[server]) {
-					user = data[server][uuid];
-					$('#players .server').last().append('<a class="playerlink" data-player="' + uuid + '"><img src="https://minotar.net/avatar/' + user + '/32" title="' + user + '" class="playericon" />');
-				}
+				$('#players .row').last().append('<div class="server" data-server="' + server + '"><h4>' + server + '</h4><span></span></div>');
 				i++;
 			}
+			updatePlayers(data);
 		});
 	}
 	
 	// When the data needs to be updated
 	function update(lastUpdate) {
-		
+		query('/api/getservers', updatePlayers);
+	}
+	
+	// Player update handler
+	function updatePlayers(data) {
+		for (server in data) {
+			var entries = '';
+			for (uuid in data[server]) {
+				user = data[server][uuid];
+				entries += '<a class="playerlink" data-player="' + uuid + '"><img src="https://minotar.net/avatar/' + user + '/32" title="' + user + '" class="playericon" />';
+			}
+			$('#players .server[data-server="' + server + '"] span').html(entries);
+		}
 	}
 	
 	// Player search handler
 	$('#players .search').submit(function(e) {
 		query('/api/getuuid?username=' + $(this).find('input[name="player"]').val(), function(data) {
-			if ("uuid" in data) showPlayer(data.uuid);
+			if (data) showPlayer(data.uuid);
 		});
 		e.preventDefault();
 	});
